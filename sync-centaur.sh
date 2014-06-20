8a552d99b09807fb20fe90735a3ffd358edd9de1
ph4r05 <ph4r05@gmail.com>
2014-06-20 18:32:01 +0200

    Sync scripts

--- sync-centaur.sh
+++ sync-centaur.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rsync -av --exclude=build** --exclude=dist** --exclude=ntl** --exclude=cube_** ./ centaur:hap/
+rsync -av --exclude=build** --exclude=dist** --exclude=ntl** --exclude=cube_** --exclude=callgrind** ./ centaur:hap/
