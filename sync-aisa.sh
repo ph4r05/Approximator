78e17eff50493015f644234fc297dd389d9fea50
ph4r05 <ph4r05@gmail.com>
2014-06-26 09:32:44 +0200

    Sync script updated.

--- sync-aisa.sh
+++ sync-aisa.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-#rsync -av --exclude=build** --exclude=dist** --exclude=ntl** --exclude=cube_** --exclude=callgrind** ./ aisa.fi.muni.cz:hap/
 rsync -av --exclude="*.bin" --exclude=ntl** --exclude=cube_** --exclude=callgrind** ./ aisa.fi.muni.cz:hap/
